c32870b36cf31cf7563e0c3e2b1c527ac4417033
akhil <akhil@A-KHILL-ESH>
2025-03-10 11:48:40 +0530

    First commit

---
+++ MavenProject/src/test/java/com/easytrack/testscript/CampaignModule.java
@@ -0,0 +1,14 @@
+package com.easytrack.testscript;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class CampaignModule {
+	
+	@Test
+	public void demo() {
+		Reporter.log("CampaignModule created",true);
+	}
+	
+
+}
